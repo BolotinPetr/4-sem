80861b25e92fadbb8ecbd157e0db0027cc1f533e
BolotinPetr <petr.bolotin@phystech.edu>
2016-04-18 12:02:18 +0300

    Add
    Добавил метод, который будет искать лидера, то есть тот объект, за которым будет следовать машина.Например, им может быть светофор или другой водитель.

--- Driver.py
+++ Driver.py
@@ -8,7 +8,21 @@ class Driver(sprite.Sprite):
         self.nerve = nerve
         self.rTime = reacTime
         self.dir = direction    #there is only one direction in oneRoad model.
-    def check (self):
-        print ''
+
+    def find_the_leader(self):
+        x0, y0, a = self.x, self.y, True
+        while a:
+            if self.y < 0:
+                a = False
+            self.x += self.dir[0]
+            self.y -= self.dir[1]
+            self.rect = Rect(self.x, self.y, ROAD_WIDTH+10*self.dir[0], ROAD_WIDTH+10*self.dir[1])
+            #driver.leaders.remove(driver)
+            for leader in self.leaders:
+                if (self.rect.contains(leader.rect))\
+                        or (leader.rect.contains(self.rect)):
+                    a = False
+                    self.leader = leader
+        self.x, self.y = x0, y0
 
 
